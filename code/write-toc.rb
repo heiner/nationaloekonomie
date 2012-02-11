bff8f1bff7192d6dd02c3f63897a2b4d2ed0e19a
Heinrich Kuettler <heinrich.kuettler@gmail.com>
2012-02-11 17:24:04 +0100

    Fixed a bug in the TOC hierarchy

--- code/write-toc.rb
+++ code/write-toc.rb
@@ -42,7 +42,9 @@ TOCLIST.each do |title, page|
   last_page = page
 end
 
-$counter = 0
+# pp TOC
+
+$counter = 1
 
 def handle_level(level, toc, xml)
   loop do
@@ -55,8 +57,8 @@ def handle_level(level, toc, xml)
       xml.navLabel { xml.text(title) }
       xml.content(:src=>"book-#{data}.html")
 
-      if l > level
-        handle_level(l, toc, xml)
+      if !toc.empty? and toc[0][0] > level
+        handle_level(toc[0][0], toc, xml)
       end
     end
   end
@@ -64,29 +66,29 @@ end
 
 
 if __FILE__ == $0
-
-$KCODE = "UTF8"
-
-File.open("output/toc.ncx", "w") do |f|
-  xml = Builder::XmlMarkup.new(:target=>f, :indent=>2)
-  xml.instruct!
-  xml.declare!( :DOCTYPE, :ncx, :PUBLIC, "-//NISO//DTD ncx 2005-1//EN",
-                "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd" )
-  xml.ncs(:xmlns=>"http://www.daisy.org/z3986/2005/ncx/", :version=>"2005-1") do
-    xml.head do
-      xml.meta :name=>"dtb:uid",   :content=>"3-88405-023-0"
-      xml.meta :name=>"dtb:depth", :content=>4
-      xml.meta :name=>"dtb:totalPageCount", :content=>0
-      xml.meta :name=>"dtb:maxPageNumber",  :content=>0
-    end
-    xml.docTitle  { xml.text "Nationalökonomie" }
-    xml.docAuthor { xml.text "Ludwig von Mises" }
-    xml.navMap do
-      handle_level(0, TOC, xml)
+  $KCODE = "UTF8"
+  File.open("output/toc.ncx", "w") do |f|
+    xml = Builder::XmlMarkup.new(:target=>f, :indent=>2)
+    xml.instruct!
+    xml.declare!( :DOCTYPE, :ncx, :PUBLIC, "-//NISO//DTD ncx 2005-1//EN",
+                  "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd" )
+    xml.ncs(:xmlns=>"http://www.daisy.org/z3986/2005/ncx/", :version=>"2005-1") do
+      xml.head do
+        xml.meta :name=>"dtb:uid",   :content=>"3-88405-023-0"
+        xml.meta :name=>"dtb:depth", :content=>4
+        xml.meta :name=>"dtb:totalPageCount", :content=>0
+        xml.meta :name=>"dtb:maxPageNumber",  :content=>0
+      end
+      xml.docTitle  { xml.text "Nationalökonomie" }
+      xml.docAuthor { xml.text "Ludwig von Mises" }
+      xml.navMap do
+        xml.navPoint(:id=>"navPoint-1", :playOrder=>1) do
+          xml.navLabel { xml.text "Nationalökonomie" }
+          xml.content( :src=>"book-front.html" )
+        end
+        handle_level(0, TOC, xml)
+      end
     end
   end
-end
-
-# puts toc.select { |type, name, title| type == CHAPTER }.length
-
+  # puts toc.select { |type, name, title| type == CHAPTER }.length
 end
