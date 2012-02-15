182be8f5e7d3dbcf5c4e2cafc65a8457e48042d4
Heinrich Kuettler <heinrich.kuettler@gmail.com>
2012-02-15 14:16:24 +0100

    Added additional <h2> tags for chapters

--- code/add-header-tags.rb
+++ code/add-header-tags.rb
@@ -12,18 +12,18 @@ require 'write-toc'
 TOC.each do |level, data, title|
   filename = "../src/OEBPS/book-#{data}.html"
 
+  start = title[0..5]
+  ending = title[-5..-1]
+  start.gsub!("(", "\\(")
+  start.gsub!(")", "\\)")
+  ending.gsub!("(", "\\(")
+  ending.gsub!(")", "\\)")
+
   case level
   when CHAPTER
-    regex = /<p>\s*<i>\s*#{title}\s*<\/i>\s*<\/p>/
-    replacement = "<h2>#{title}</h2>"
+    regex = /<p>\s*<i>\s*(#{start}[^<>]*#{ending}.?)\s*<\/i>\s*<\/p>/i
+    replacement = "<h2>\\1</h2>"
   when SECTION
-    start = title[0..5]
-    ending = title[-5..-1]
-    start.gsub!("(", "\\(")
-    start.gsub!(")", "\\)")
-    ending.gsub!("(", "\\(")
-    ending.gsub!(")", "\\)")
-
     regex = /<p>\s*(#{start}[^<>]*#{ending}.?)\s*<\/p>/
     replacement = "<h3>\\1</h3>"
   when EXKURS
@@ -45,13 +45,17 @@ TOC.each do |level, data, title|
         f.write(content)
       end
     else
-      # puts "No success for #{level}, #{data}, #{title}" if level == CHAPTER
-
-      if level == SECTION
-        if not content =~ /<h3>\s*(#{start}[^<>]*#{ending}.?)\s*<\/h3>/
-          puts "No success for #{data}, \"#{title}\""
+      if level == CHAPTER
+        if not content =~ /<h2>\s*(#{start}[^<>]*#{ending}.?)\s*<\/h2>/i
+          puts "No success for #{level}, #{data}, #{title}"
         end
       end
+
+      # if level == SECTION
+      #   if not content =~ /<h3>\s*(#{start}[^<>]*#{ending}.?)\s*<\/h3>/
+      #     puts "No success for #{data}, \"#{title}\""
+      #   end
+      # end
     end
   rescue Errno::ENOENT => e
     puts e
