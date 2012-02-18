83517b5c5dcff877691de3632ea5fbd6960252d6
Heinrich Kuettler <heinrich.kuettler@gmail.com>
2012-02-18 16:42:16 +0100

    Fixed paragraph breaks accross old file divisions

--- code/fix-some-linebreaks.rb
+++ code/fix-some-linebreaks.rb
@@ -6,6 +6,24 @@ term = Termios::getattr( $stdin )
 term.c_lflag &= ~Termios::ICANON
 Termios::setattr( $stdin, Termios::TCSANOW, term )
 
+### Regex for hyphenated word
+# regex = %r{ ([A-ZÄÖÜ]?[a-zäöüß]+)-
+#             \s* </p> \s* (?:<p> \s* </p> \s*)?
+#             (<!--\ page\ [0-9]+\ -->)
+#             \s* <p> \s*
+#             ([a-zäöü]+)
+#           }x
+
+### Regex for broken-up sentence
+# regex = %r{ ([A-ZÄÖÜ]?[a-zäöüß]+\ )
+#             \s* </p> \s* (?:<p> \s* </p> \s*)?
+#             (<!--\ page\ [0-9]+\ -->)
+#             \s* <p> \s*
+#             ([a-zäöü]+)
+#           }x
+
+### Regex for broken-up paragraphs
+### Should probably not be "fixed"!
 regex = %r{ ([A-ZÄÖÜ]?[a-zäöüß]+\.)
             \s* </p> \s* (?:<p> \s* </p> \s*)?
             (<!--\ page\ [0-9]+\ -->)
@@ -17,6 +35,8 @@ ARGV.each do |fn|
   contents = File.read(fn)
   success = \
   contents.gsub!(regex) do |match|
+    # replacement = $1 + $2 + $3
+    # replacement = $1 + $2 + " " + $3
     replacement = $1 + " " + $2 + " " + $3
 
     print "Change \"#{match}\" to \"#{replacement}\"? "
